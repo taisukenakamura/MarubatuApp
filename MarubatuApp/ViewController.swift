f278e25127a81a778c2e488afd82a6588a69f9dd
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-04 09:38:39 +0900

    問題表示処理の作成

--- MarubatuApp/ViewController.swift
+++ MarubatuApp/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+//    クイズの問題を表示
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    //    現在のクイズ番号を管理
+    var currentQuestionNum : Int = 0
+    
+    
+    let questions: [[String : Any]] = [
+        
+        ["question" : "iPhoneアプリを開発する統合環境はZcodeである",
+         "answer"   : false
+        ],
+        [ "question": "Xcode画面の右側にはユーティリティーズがある",
+          "answer": true
+        ],
+        [ "question": "UILabelは文字列を表示する際に利用する",
+          "answer": true
+        ],
+        [ "question": "ドラえもんは常に地面から浮いている",
+          "answer": true
+        ],
+        
+]
+    
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+//    まるボタン
+    @IBAction func tappedYesButton(_ sender: UIButton) {
+    }
+    
+//    バツボタン
+    @IBAction func tappedNoButton(_ sender: UIButton) {
+    }
+    
+    func showQuestion(){
+        let question = questions[currentQuestionNum]
+        
+        if let que  = question["question"] as? String{
+            questionLabel.text = que
+        }
+    }
+    
 }
 
